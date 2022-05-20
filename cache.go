cd9ed366cec369bf9dde51b38b09e416d40a74f7
Demid <dagdog2506@gmail.com>
2022-05-20 19:48:17 +0500

    fix: hw5.2 v.2

--- cache.go
+++ cache.go
@@ -20,7 +20,7 @@ func NewCache() Cache {
 func (c *Cache) Get(key string) (string, bool) {
 
 	for _, item := range c.storage {
-		if item.key == key && item.expirationTime.Before(time.Now()) {
+		if item.key == key && (item.expirationTime.Before(time.Now()) || !item.willExpired) {
 			return item.value, true
 		}
 	}
@@ -29,7 +29,27 @@ func (c *Cache) Get(key string) (string, bool) {
 }
 
 func (c *Cache) Put(key, value string) {
-	c.storage = append(c.storage, Key{key: key, value: value, willExpired: false})
+
+	needOverwrite := false
+	for _, nonExpKey := range c.Keys() {
+		if nonExpKey == key {
+			needOverwrite = true
+		}
+
+	}
+
+	if needOverwrite {
+		for _, item := range c.storage {
+			if item.key == key {
+				item.value = value
+				item.willExpired = false
+			}
+		}
+
+	} else {
+		c.storage = append(c.storage, Key{key: key, value: value, willExpired: false})
+
+	}
 
 }
 
@@ -46,5 +66,27 @@ func (c *Cache) Keys() []string {
 }
 
 func (c *Cache) PutTill(key, value string, deadline time.Time) {
-	c.storage = append(c.storage, Key{key: key, value: value, willExpired: true, expirationTime: deadline})
+
+	time.Sleep(3)
+	needOverwrite := false
+	for _, nonExpKey := range c.Keys() {
+		if nonExpKey == key {
+			needOverwrite = true
+		}
+
+	}
+
+	if needOverwrite {
+		for _, item := range c.storage {
+			if item.key == key {
+				item.value = value
+				item.willExpired = true
+				item.expirationTime = deadline
+			}
+		}
+
+	} else {
+		c.storage = append(c.storage, Key{key: key, value: value, willExpired: true, expirationTime: deadline})
+
+	}
 }

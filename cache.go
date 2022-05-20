466f09c518167fcf5dea70a40f7cccdeca41ec16
Demid <dagdog2506@gmail.com>
2022-05-20 21:18:41 +0500

    fix:bugs

--- cache.go
+++ cache.go
@@ -3,7 +3,7 @@ package cache
 import "time"
 
 type Cache struct {
-	storage []Key
+	Storage []Key
 }
 
 type Key struct {
@@ -19,7 +19,7 @@ func NewCache() Cache {
 
 func (c *Cache) Get(key string) (string, bool) {
 
-	for _, item := range c.storage {
+	for _, item := range c.Storage {
 		if item.key == key && (item.expirationTime.Before(time.Now()) || !item.willExpired) {
 			return item.value, true
 		}
@@ -31,23 +31,25 @@ func (c *Cache) Get(key string) (string, bool) {
 func (c *Cache) Put(key, value string) {
 
 	needOverwrite := false
-	for _, nonExpKey := range c.Keys() {
+	rangeM := c.Keys()
+	for _, nonExpKey := range rangeM {
 		if nonExpKey == key {
 			needOverwrite = true
 		}
 
 	}
 
-	if needOverwrite {
-		for _, item := range c.storage {
-			if item.key == key {
-				item.value = value
-				item.willExpired = false
-			}
-		}
+	for i := range c.Storage {
 
-	} else {
-		c.storage = append(c.storage, Key{key: key, value: value, willExpired: false})
+		if c.Storage[i].key == key {
+			c.Storage[i].value = value
+			c.Storage[i].willExpired = false
+
+		}
+	}
+
+	if !needOverwrite {
+		c.Storage = append(c.Storage, Key{key: key, value: value, willExpired: false})
 
 	}
 
@@ -56,7 +58,7 @@ func (c *Cache) Put(key, value string) {
 func (c *Cache) Keys() []string {
 	var keys []string
 
-	for _, item := range c.storage {
+	for _, item := range c.Storage {
 		if item.expirationTime.Before(time.Now()) && !item.willExpired {
 			keys = append(keys, item.key)
 		}
@@ -76,7 +78,7 @@ func (c *Cache) PutTill(key, value string, deadline time.Time) {
 	}
 
 	if needOverwrite {
-		for _, item := range c.storage {
+		for _, item := range c.Storage {
 			if item.key == key {
 				item.value = value
 				item.willExpired = true
@@ -85,7 +87,7 @@ func (c *Cache) PutTill(key, value string, deadline time.Time) {
 		}
 
 	} else {
-		c.storage = append(c.storage, Key{key: key, value: value, willExpired: true, expirationTime: deadline})
+		c.Storage = append(c.Storage, Key{key: key, value: value, willExpired: true, expirationTime: deadline})
 
 	}
 }

1ac03b3136683774ae35c4db64d281c5a0cdeb82
Demid <dagdog2506@gmail.com>
2022-05-20 19:11:08 +0500

    Hw 5.2 done.

--- cache.go
+++ cache.go
@@ -3,21 +3,48 @@ package cache
 import "time"
 
 type Cache struct {
+	storage []Key
+}
+
+type Key struct {
+	key            string
+	value          string
+	expirationTime time.Time
+	willExpired    bool
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+
+	for _, item := range c.storage {
+		if item.key == key && item.expirationTime.Before(time.Now()) {
+			return item.key, true
+		}
+	}
+
+	return "", false
+}
+
+func (c *Cache) Put(key, value string) {
+	c.storage = append(c.storage, Key{key: key, value: value, willExpired: false})
 
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Keys() []string {
+	var keys []string
+
+	for _, item := range c.storage {
+		if item.expirationTime.Before(time.Now()) && !item.willExpired {
+			keys = append(keys, item.key)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) Keys() []string {
-}
-
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.storage = append(c.storage, Key{key: key, value: value, willExpired: true, expirationTime: deadline})
 }
